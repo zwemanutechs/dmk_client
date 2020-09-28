8fc07de3731a88f0e386ed3fbc4b62f0d590e1a2
zwehtatnaing <zwehtatnaing>
2020-09-28 14:04:58 +0800

    dashboard update

--- src/components/home/container/home.jsx
+++ src/components/home/container/home.jsx
@@ -1,5 +1,16 @@
 import React, {Component} from 'react';
 import AppLineChart from "../../../shared/charts/line/container/lineChart";
+import InfiniteCarousel from 'react-leaf-carousel';
+import "./home.css";
+import Card from "@material-ui/core/Card";
+import Grid from "@material-ui/core/Grid";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import ErrorOutlineOutlinedIcon from '@material-ui/icons/ErrorOutlineOutlined';
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import CardHeader from "@material-ui/core/CardHeader";
+import Avatar from "@material-ui/core/Avatar";
 
 class Home extends Component{
 
@@ -9,7 +20,253 @@ class Home extends Component{
 
     render() {
         return (
-            <AppLineChart/>
+           <Grid container direction={"row"} spacing={2}>
+               <Grid item xs={12}>
+                   <Card style={{margin: 10, maxWidth: 1424}}>
+                       <InfiniteCarousel
+                           breakpoints={[
+                               {
+                                   breakpoint: 500,
+                                   settings: {
+                                       slidesToShow: 1,
+                                       slidesToScroll: 1,
+                                   },
+                               },
+                               {
+                                   breakpoint: 768,
+                                   settings: {
+                                       slidesToShow: 1,
+                                       slidesToScroll: 1,
+                                   },
+                               },
+                               {
+                                   breakpoint: 1024,
+                                   settings: {
+                                       slidesToShow: 5,
+                                       slidesToScroll: 5,
+                                   },
+                               },
+                               {
+                                   breakpoint: 1200,
+                                   settings: {
+                                       slidesToShow: 5,
+                                       slidesToScroll: 5,
+                                   },
+                               },
+                               {
+                                   breakpoint: 3000,
+                                   settings: {
+                                       slidesToShow: 5,
+                                       slidesToScroll: 5,
+                                   },
+                               },
+                           ]}
+                           dots={false}
+                           showSides={false}
+                           sidesOpacity={0.1}
+                           sideSize={0.1}
+                           slidesToScroll={5}
+                           slidesToShow={5}
+                           scrollOnDevice={true}
+                           autoCycle={true}
+                           arrows={false}
+                           slidesSpacing={10}
+                           cycleInterval={5000}
+                       >
+                           <div>
+                               <AppLineChart
+                                   key={1}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 5000, label: 'Max'},{value: 2000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                           <div>
+                               <AppLineChart
+                                   key={2}
+                                   url={'https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25'}
+                                   keys={['confirmed', 'deaths', 'recovered']}
+                                   width={280}
+                                   height={280}
+                                   xDataKey={'date'}
+                                   referenceLineData={[{value: 6000, label: 'Max'},{value: 1000, label: 'Min'}]}
+                               />
+                           </div>
+                       </InfiniteCarousel>
+                   </Card>
+               </Grid>
+               <Grid item xs={12} md={4}>
+                   <Card style={{margin: 10}}>
+                       <CardHeader
+                           avatar={
+                               <Avatar aria-label="recipe">
+                                   D
+                               </Avatar>
+                           }
+                           title="Degreasing"
+                           subheader="September 14, 2020"
+                       />
+                       <List dense={true}>
+                           <ListItem>
+                               <ListItemIcon>
+                                   <ErrorOutlineOutlinedIcon color="secondary"/>
+                               </ListItemIcon>
+                               <ListItemText
+                                   primary="System detect the high value."
+                               />
+                           </ListItem>
+                       </List>
+                   </Card>
+               </Grid>
+               <Grid item xs={12} md={4}>
+                   <Card style={{margin: 10}}>
+                       <CardHeader
+                           avatar={
+                               <Avatar aria-label="recipe">
+                                   O
+                               </Avatar>
+                           }
+                           title="Oven"
+                           subheader="September 14, 2020"
+                       />
+                       <List dense={true}>
+                           <ListItem>
+                               <ListItemIcon>
+                                   <ErrorOutlineOutlinedIcon color="secondary"/>
+                               </ListItemIcon>
+                               <ListItemText
+                                   primary="System detect the high value."
+                               />
+                           </ListItem>
+                       </List>
+                   </Card>
+               </Grid>
+               <Grid item xs={12} md={4}>
+                   <Card style={{margin: 10}}>
+                       <CardHeader
+                           avatar={
+                               <Avatar aria-label="recipe">
+                                   RDI
+                               </Avatar>
+                           }
+                           title="Rinse DI"
+                           subheader="September 14, 2020"
+                       />
+                       <List dense={true}>
+                           <ListItem>
+                               <ListItemIcon>
+                                   <ErrorOutlineOutlinedIcon color="secondary"/>
+                               </ListItemIcon>
+                               <ListItemText
+                                   primary="System detect the high value."
+                               />
+                           </ListItem>
+                       </List>
+                   </Card>
+               </Grid>
+           </Grid>
         );
     }
 }
--- src/shared/charts/line/container/lineChart.jsx
+++ src/shared/charts/line/container/lineChart.jsx
@@ -1,11 +1,33 @@
-import React, { Component, useEffect } from 'react';
+import React, {Component, PureComponent, useEffect} from 'react';
 import Skeleton from '@material-ui/lab/Skeleton';
 import { lineChartFetchData } from '../actions/lineChart-actions';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ReferenceLine } from 'recharts';
 import {colours} from '../../chart-constants';
 import {connect} from "react-redux";
-import Card from "@material-ui/core/Card";
-import CardContent from "@material-ui/core/CardContent";
+
+class CustomizedLabel extends PureComponent {
+    render() {
+        const {
+            x, y, stroke, value,
+        } = this.props;
+
+        return <text x={x} y={y} dy={-4} fill={stroke} fontSize={12} textAnchor="middle">{value}</text>;
+    }
+}
+
+class CustomizedAxisTick extends PureComponent {
+    render() {
+        const {
+            x, y, stroke, payload,
+        } = this.props;
+
+        return (
+            <g transform={`translate(${x},${y})`}>
+                <text x={0} y={0} dy={6} textAnchor="end" fill="#666" transform="rotate(-35)">{payload.value}</text>
+            </g>
+        );
+    }
+}
 
 function AppLineChart(props) {
 
@@ -14,24 +36,27 @@ function AppLineChart(props) {
     }, []);
 
     function fetchData () {
-        props.lineChartFetchData('https://covidapi.info/api/v1/country/SGP/timeseries/2020-03-15/2020-04-25', ['confirmed', 'deaths', 'recovered'])
+        props.lineChartFetchData(props.url, props.keys)
     }
 
     return (
         props.loading
-            ? <Skeleton variant="rect" width={300} height={300} />
-            :<Card style={{maxWidth: 300}}>
-                    <LineChart width={290} height={300} data={props.data} style={{margin: 5}}>
+            ? <Skeleton variant="rect" width={props.width} height={props.height}/>
+            :<LineChart width={props.width} height={props.height} data={props.data} margin={{top: 10, right: 10, left: 10, bottom: 10}}>
                         <CartesianGrid strokeDasharray="3 3" />
-                        <XAxis dataKey="date" />
-                        <YAxis />
+                        <XAxis dataKey={props.xDataKey} fontSize={12} height={60} allowDuplicatedCategory={false} tick={<CustomizedAxisTick />}/>
+                        <YAxis/>
+                        {
+                            Array.isArray(props.referenceLineData) && props.referenceLineData.length > 0
+                                ? props.referenceLineData.map((refLine, ri) => ( <ReferenceLine key={`r${ri}`} y={refLine.value} label={refLine.label} stroke="red" />))
+                                : null
+                        }
                         <Tooltip />
                         {
-                            props.legendKeys.map((dataKey, i) => <Line key={i} type="monotone" dataKey={dataKey} stroke={colours[i]}  activeDot={{ r: 8 }}/>)
+                            props.legendKeys.map((dataKey, i) => <Line key={`l${i}`} type="monotone" dataKey={dataKey} stroke={colours[i]} strokeWidth={1} label={<CustomizedLabel />}/>)
                         }
 
-                    </LineChart>
-            </Card>
+            </LineChart>
     );
 }
 
